30dcd297f9f2074faa39f39e9c1671c9c9f8f31d
Claytorpedo <claytorpedo@gmail.com>
2019-05-27 00:13:11 -0700

    Changes to ShapeContainer. Less indirection.
    Will be able to change the collision interface to
    take a lightweight ShapeRef rather than a full container.
    This should avoid a lot of unnecessary heap allocation.

--- geom/shapes/ShapeContainer.hpp
+++ geom/shapes/ShapeContainer.hpp
@@ -2,47 +2,91 @@
 #ifndef INCLUDE_GEOM_SHAPE_CONTAINER_HPP
 #define INCLUDE_GEOM_SHAPE_CONTAINER_HPP
 
-#include "Shape.hpp"
 #include "Rectangle.hpp"
 #include "Polygon.hpp"
 #include "Circle.hpp"
+#include <variant>
 
 namespace geom {
+
 	enum class ShapeType {
-		EMPTY,
 		RECTANGLE,
 		POLYGON,
 		CIRCLE,
 	};
 
-	class ShapeContainer {
+	class ShapeRef {
 	public:
-		~ShapeContainer() { delete shape_; }
-		ShapeContainer() : type_(ShapeType::EMPTY), shape_(nullptr) {}
-		ShapeContainer(const Rect& r)    : type_(ShapeType::RECTANGLE), rect_(new Rect(r)),     shape_(rect_) {}
-		ShapeContainer(const Polygon& p) : type_(ShapeType::POLYGON),   poly_(new Polygon(p)),  shape_(poly_) {}
-		ShapeContainer(const Circle& c)  : type_(ShapeType::CIRCLE),    circle_(new Circle(c)), shape_(circle_) {}
-		ShapeContainer(Rect&& r)    : type_(ShapeType::RECTANGLE), rect_(new Rect(std::move(r))),     shape_(rect_) {}
-		ShapeContainer(Polygon&& p) : type_(ShapeType::POLYGON),   poly_(new Polygon(std::move(p))),  shape_(poly_) {}
-		ShapeContainer(Circle&& c)  : type_(ShapeType::CIRCLE),    circle_(new Circle(std::move(c))), shape_(circle_) {}
-		ShapeContainer(const ShapeContainer& other);
-		ShapeContainer(ShapeContainer&& other);
-		ShapeContainer& operator=(const ShapeContainer& other);
-		ShapeContainer& operator=(ShapeContainer&& other);
+		constexpr ShapeRef(Rect& r) noexcept : type_{ShapeType::RECTANGLE}, shape_{&r} {}
+		constexpr ShapeRef(Polygon& p) noexcept : type_{ShapeType::POLYGON}, shape_{&p} {}
+		constexpr ShapeRef(Circle& c) noexcept : type_{ShapeType::CIRCLE}, shape_{&c} {}
 
-		inline const ShapeType& type()   const { return type_; }
-		inline const Shape&     shape()  const { return *shape_; }
-		inline const Rect&      rect()   const { return *rect_; }
-		inline const Polygon&   poly()   const { return *poly_; }
-		inline const Circle&    circle() const { return *circle_; }
+		ShapeRef() = delete;
+		ShapeRef(const ShapeRef&) = default;
+		ShapeRef(ShapeRef&&) = default;
+		ShapeRef& operator=(const ShapeRef&) = default;
+		ShapeRef& operator=(ShapeRef&&) = default;
+
+		constexpr const ShapeType& type() const noexcept { return type_; }
+		constexpr const Shape& shape() const noexcept { return *shape_; }
+		constexpr const Rect& rect() const noexcept { return static_cast<Rect&>(*shape_); }
+		constexpr const Polygon& poly() const noexcept { return static_cast<Polygon&>(*shape_); }
+		constexpr const Circle& circle() const noexcept { return static_cast<Circle&>(*shape_); }
+		constexpr Rect& rect() noexcept { return static_cast<Rect&>(*shape_); }
+		constexpr Polygon& poly() noexcept { return static_cast<Polygon&>(*shape_); }
+		constexpr Circle& circle() noexcept { return static_cast<Circle&>(*shape_); }
+	protected:
+		constexpr ShapeRef(ShapeType t) noexcept : type_{t} {}
+		constexpr void setShape(Shape& s) noexcept { shape_ = &s; }
 	private:
 		ShapeType type_;
-		union {
-			Rect*    rect_;
-			Polygon* poly_;
-			Circle*  circle_;
-		};
-		Shape* shape_;
+		Shape* shape_{nullptr};
+	};
+
+	// Hold any shape with type information.
+	class ShapeContainer : public ShapeRef {
+	public:
+		ShapeContainer(Rect r) : ShapeRef{ShapeType::RECTANGLE}, shape_{std::move(r)} {
+			ShapeRef::setShape(std::get<Rect>(shape_));
+		}
+		ShapeContainer(Polygon p) : ShapeRef{ShapeType::POLYGON}, shape_{std::move(p)} {
+			ShapeRef::setShape(std::get<Polygon>(shape_));
+		}
+		ShapeContainer(Circle c) : ShapeRef{ShapeType::CIRCLE}, shape_{std::move(c)} {
+			ShapeRef::setShape(std::get<Circle>(shape_));
+		}
+
+		ShapeContainer(const ShapeContainer& o) : ShapeRef{o.type()}, shape_{o.shape_} {
+			setShape();
+		}
+		ShapeContainer(ShapeContainer&& o) : ShapeRef{o.type()}, shape_{std::move(o.shape_)} {
+			setShape();
+		}
+		ShapeContainer& operator=(const ShapeContainer& o) {
+			if (this != &o) {
+				shape_ = o.shape_;
+				setShape();
+			}
+			return *this;
+		}
+		ShapeContainer& operator=(ShapeContainer&& o) {
+			if (this != &o) {
+				shape_ = std::move(o.shape_);
+				setShape();
+			}
+			return *this;
+		}
+
+	private:
+		void setShape() {
+			switch (type()) {
+			case ShapeType::RECTANGLE: ShapeRef::setShape(std::get<Rect>(shape_)); break;
+			case ShapeType::POLYGON: ShapeRef::setShape(std::get<Polygon>(shape_)); break;
+			case ShapeType::CIRCLE: ShapeRef::setShape(std::get<Circle>(shape_)); break;
+			}
+		}
+
+		std::variant<Rect, Polygon, Circle> shape_;
 	};
 }
 
