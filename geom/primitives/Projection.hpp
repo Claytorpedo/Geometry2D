54f8537f1e9f7e4da7bbd70c0b03fc9cec0b090f
Claytorpedo <claytorpedo@gmail.com>
2019-06-01 18:58:59 -0700

    Made Projection constexpr.

--- geom/primitives/Projection.hpp
+++ geom/primitives/Projection.hpp
@@ -7,10 +7,10 @@
 // Simple struct for containing a min and max value.
 namespace geom {
 	struct Projection {
-		gFloat min, max;
-		Projection() : min(0), max(0) {}
-		Projection(gFloat min, gFloat max) : min(min), max(max) {}
-		inline Projection& operator+=(const gFloat& v) { min += v; max += v; return *this; }
+		gFloat min{0}, max{0};
+		Projection() = default;
+		constexpr Projection(gFloat min, gFloat max) noexcept : min{min}, max{max} {}
+		constexpr Projection& operator+=(const gFloat& v) noexcept { min += v; max += v; return *this; }
 	};
 }
 #endif // INCLUDE_GEOM_PROJECTION_HPP
