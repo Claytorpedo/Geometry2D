e87e5e3f96a406a28ad6aff405e5a337600cc3a0
Claytorpedo <claytorpedo@gmail.com>
2019-06-16 22:29:15 -0700

    Added Rect operator== and nodiscard attribute.

--- geom/shapes/Rectangle.hpp
+++ geom/shapes/Rectangle.hpp
@@ -21,32 +21,34 @@ namespace geom {
 		Rect& operator=(Rect&& rect) = default;
 		~Rect() = default;
 
+		[[nodiscard]] constexpr bool operator==(const Rect& o) noexcept { return x == o.x && y == o.y && w == o.w && h == o.h; }
+
 		// Translate by a vector.
 		constexpr Rect& operator+=(const Coord2& c) noexcept { x += c.x; y += c.y; return *this; }
 		// Translate by a vector.
-		Rect operator+(const Coord2& c) const noexcept { return Rect{x + c.x, y + c.y, w, h}; }
+		[[nodiscard]] Rect operator+(const Coord2& c) const noexcept { return Rect{x + c.x, y + c.y, w, h}; }
 
-		constexpr Coord2 position() const noexcept { return Coord2(x, y); }
-		constexpr Coord2 center() const noexcept { return Coord2(x + w*0.5f, y + h*0.5f); }
-		constexpr gFloat center_x() const noexcept { return x + w*0.5f; }
-		constexpr gFloat center_y() const noexcept { return y + h*0.5f; }
-		constexpr gFloat width() const noexcept { return w; }
-		constexpr gFloat height() const noexcept { return h; }
-		virtual gFloat left() const noexcept { return x; }
-		virtual gFloat right() const noexcept { return x + w; }
-		virtual gFloat top() const noexcept { return y; }
-		virtual gFloat bottom() const noexcept { return y + h; }
+		[[nodiscard]] constexpr Coord2 position() const noexcept { return Coord2(x, y); }
+		[[nodiscard]] constexpr Coord2 center() const noexcept { return Coord2(x + w*0.5f, y + h*0.5f); }
+		[[nodiscard]] constexpr gFloat center_x() const noexcept { return x + w*0.5f; }
+		[[nodiscard]] constexpr gFloat center_y() const noexcept { return y + h*0.5f; }
+		[[nodiscard]] constexpr gFloat width() const noexcept { return w; }
+		[[nodiscard]] constexpr gFloat height() const noexcept { return h; }
+		[[nodiscard]] virtual gFloat left() const noexcept { return x; }
+		[[nodiscard]] virtual gFloat right() const noexcept { return x + w; }
+		[[nodiscard]] virtual gFloat top() const noexcept { return y; }
+		[[nodiscard]] virtual gFloat bottom() const noexcept { return y + h; }
 
-		constexpr Coord2 topLeft() const noexcept { return {x, y}; }
-		constexpr Coord2 topRight() const noexcept { return {x + w, y}; }
-		constexpr Coord2 bottomLeft() const noexcept { return {x, y + h}; }
-		constexpr Coord2 bottomRight() const noexcept { return {x + w, y + h}; }
+		[[nodiscard]] constexpr Coord2 topLeft() const noexcept { return {x, y}; }
+		[[nodiscard]] constexpr Coord2 topRight() const noexcept { return {x + w, y}; }
+		[[nodiscard]] constexpr Coord2 bottomLeft() const noexcept { return {x, y + h}; }
+		[[nodiscard]] constexpr Coord2 bottomRight() const noexcept { return {x + w, y + h}; }
 
-		bool isInside(const Rect& o) const noexcept; // See if this rectangle is contained by another one.
+		[[nodiscard]] bool isInside(const Rect& o) const noexcept; // See if this rectangle is contained by another one.
 
-		virtual Projection getProjection(const Coord2& axis) const;
-		virtual Coord2 getClosestTo(const Coord2 point) const; // Gets closest corner of the rectangle.
-		virtual Polygon toPoly() const;
+		[[nodiscard]] virtual Projection getProjection(const Coord2& axis) const;
+		[[nodiscard]] virtual Coord2 getClosestTo(const Coord2 point) const; // Gets closest corner of the rectangle.
+		[[nodiscard]] virtual Polygon toPoly() const;
 	};
 }
 #endif // INCLUDE_GEOM_RECT_HPP
