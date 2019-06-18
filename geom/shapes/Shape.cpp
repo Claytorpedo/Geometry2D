d691258a1976d41c8b3441c166918b4b5c6da172
Claytorpedo <claytorpedo@gmail.com>
2019-06-17 22:33:34 -0700

    Cleaned up scope.

--- geom/shapes/Shape.cpp
+++ geom/shapes/Shape.cpp
@@ -6,30 +6,29 @@
 #include "../units.hpp"
 #include "../primitives/Projection.hpp"
 
-namespace geom {
-	gFloat Shape::side(Direction side) const {
-		if (side == Direction::LEFT)
-			return left();
-		if (side == Direction::RIGHT)
-			return right();
-		if (side == Direction::UP)
-			return top();
-		if (side == Direction::DOWN)
-			return bottom();
-		DBG_ERR("Error: Invalid direction to get side.");
-		return 0;
-	}
+using namespace geom;
 
-	Projection Shape::getProjection(const Coord2& axis) const {
-		Polygon p(this->toPoly());
-		return p.getProjection(axis);
+gFloat Shape::side(Direction side) const {
+	switch (side) {
+		case Direction::LEFT: return left();
+		case Direction::RIGHT: return right();
+		case Direction::UP: return top();
+		case Direction::DOWN: return bottom();
+		default: break;
 	}
+	DBG_ERR("Error: Invalid direction to get side.");
+	return 0;
+}
 
-	Rect Shape::getAABB() const {
-		return Rect(left(), top(), right() - left(), bottom() - top());
-	}
+Projection Shape::getProjection(const Coord2& axis) const {
+	Polygon p(this->toPoly());
+	return p.getProjection(axis);
+}
 
-	Coord2 Shape::getClosestTo(const Coord2& point) const {
-		return this->toPoly().getClosestTo(point);
-	}
+Rect Shape::getAABB() const {
+	return Rect(left(), top(), right() - left(), bottom() - top());
+}
+
+Coord2 Shape::getClosestTo(const Coord2& point) const {
+	return this->toPoly().getClosestTo(point);
 }
