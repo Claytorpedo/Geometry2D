a02cf42093c1de06a897c448f7fd13b6559efebf
Claytorpedo <claytorpedo@gmail.com>
2019-09-24 22:50:12 -0700

    debug_logger: better organization/naming.

--- geom/debug_logger.hpp
+++ geom/debug_logger.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #if defined(DEBUG) || defined(_DEBUG)
 
+#include <iostream>
+#include <string_view>
+
 /* --------------- Set logging colours --------------- */
 #ifdef NO_CONSOLE_COLOUR
 #define ERR_COL  ""
@@ -15,23 +18,21 @@
 #endif
 /* --------------------------------------------------- */
 
-#include <iostream>
-#include <string>
 #define DBG_ERR(x)  do { std::cerr << ERR_COL  "ERROR:"   RST_COL " " << x << "\n"; } while (0)
 #define DBG_WARN(x) do { std::cerr << WARN_COL "WARNING:" RST_COL " " << x << "\n"; } while (0)
 #define DBG_LOG(x)  do { std::cout << LOG_COL  "LOG:"     RST_COL " " << x << "\n"; } while (0)
 
 // Check a condition, and report to the requested stream if it is true.
 // Streams are "LOG", "WARN", and "ERR".
-#define DBG_CHECK(cond, type, str) do { \
+#define DBG_CHECK(cond, type, out) do { \
 	if (cond) {                         \
-		std::string t(type);            \
+		const std::string_view t(type); \
 		if (t == "LOG") {               \
-			DBG_LOG(str);               \
+			DBG_LOG(out);               \
 		} else if (t == "WARN") {       \
-			DBG_WARN(str);              \
+			DBG_WARN(out);              \
 		} else {                        \
-			DBG_ERR(str);               \
+			DBG_ERR(out);               \
 		}                               \
 	}                                   \
 } while(0)
