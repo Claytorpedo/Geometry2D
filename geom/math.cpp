c9c82030fd17cd5ea535ec70e1c69fe9b6ec23c4
Claytorpedo <claytorpedo@gmail.com>
2019-12-01 21:28:42 -0800

    Cleanup. Needs work on float comparisons.
    Removed clamp as that's now a standard lib function.

--- geom/math.cpp
+++ geom/math.cpp
@@ -1,35 +1,28 @@
 #include "math.hpp"
 
 #include "primitives/Ray.hpp"
-#include "units.hpp"
 
-namespace geom {
-	namespace math {
-		Coord2 closestPointOnLine(const Ray& ray, const Coord2& point) {
-			return ray.origin + (point - ray.origin).dot(ray.dir) * ray.dir;
-		}
-		Coord2 closestPointOnLine(const Coord2& line1, const Coord2& line2, const Coord2& point) {
-			const Coord2 dir(line2 - line1); // Unnormalized direction.
-			return line1 + ((point - line1).dot(dir) / dir.magnitude2()) * dir;
-		}
-		gFloat closestDistToLine(const Ray& ray, const Coord2& point) {
-			return std::abs((point - ray.origin).dot(ray.dir.perpCCW()));
-		}
-		gFloat closestDistToLine(const Coord2& line1, const Coord2& line2, const Coord2& point) {
-			return closestDistToLine(Ray(line1, (line2 - line1).normalize()), point);
-		}
+using namespace geom;
 
-		AngleResult minAngle(const Coord2& vec1, const Coord2& vec2) {
-			const gFloat dot = vec1.dot(vec2);
-			if (almostZero(dot))
-				return AngleResult::PERPENDICULAR;
-			if (dot > 0)
-				return AngleResult::ACUTE;
-			return AngleResult::OBTUSE;
-		}
+Coord2 math::closestPointOnLine(const Ray& ray, const Coord2& point) noexcept {
+	return ray.origin + (point - ray.origin).dot(ray.dir) * ray.dir;
+}
+Coord2 math::closestPointOnLine(const Coord2& line1, const Coord2& line2, const Coord2& point) noexcept {
+	const Coord2 dir(line2 - line1); // Unnormalized direction.
+	return line1 + ((point - line1).dot(dir) / dir.magnitude2()) * dir;
+}
+gFloat math::closestDistToLine(const Ray& ray, const Coord2& point) noexcept {
+	return std::abs((point - ray.origin).dot(ray.dir.perpCCW()));
+}
+gFloat math::closestDistToLine(const Coord2& line1, const Coord2& line2, const Coord2& point) noexcept {
+	return closestDistToLine(Ray(line1, (line2 - line1).normalize()), point);
+}
 
-		Coord2 reflect(const Coord2& dir, const Coord2& norm) {
-			return dir - 2.0f * norm * dir.dot(norm);
-		}
-	}
+math::AngleResult math::minAngle(const Coord2& vec1, const Coord2& vec2) noexcept {
+	const gFloat dot = vec1.dot(vec2);
+	if (almostZero(dot))
+		return AngleResult::PERPENDICULAR;
+	if (dot > 0)
+		return AngleResult::ACUTE;
+	return AngleResult::OBTUSE;
 }
--- geom/math.hpp
+++ geom/math.hpp
@@ -5,54 +5,51 @@
 #include "units.hpp"
 #include "constants.hpp"
 
-// Additional geometry math.
-namespace geom {
-	class Ray;
+// TODO: How we do floating point comparisons with tolerances here needs more consideration.
+// I should consider moving this to a separate math library project as well...
+namespace geom { class Ray; }
+namespace geom::math {
+	// Find the closest point on a line (defined by a bi-directional ray) to a given point.
+	[[nodiscard]] Coord2 closestPointOnLine(const Ray& ray, const Coord2& point) noexcept;
+	// Find the closest point on a line (made from two points) to a given point.
+	[[nodiscard]] Coord2 closestPointOnLine(const Coord2& line1, const Coord2& line2, const Coord2& point) noexcept;
+	// Find the closest distance to a line (defined by a bi-directional ray) to a given point.
+	[[nodiscard]] gFloat closestDistToLine(const Ray& ray, const Coord2& point) noexcept;
+	// Find the closest distance to a line (made from two points) to a given point.
+	[[nodiscard]] gFloat closestDistToLine(const Coord2& line1, const Coord2& line2, const Coord2& point) noexcept;
 
-	namespace math {
-		// Find the closest point on a line (made from a ray) to a given point.
-		Coord2 closestPointOnLine(const Ray& ray, const Coord2& point);
-		// Find the closest point on a line (made from two points) to a given point.
-		Coord2 closestPointOnLine(const Coord2& line1, const Coord2& line2, const Coord2& point);
-		// Find the closest distance to a line (made from a ray) to a given point.
-		gFloat closestDistToLine(const Ray& ray, const Coord2& point);
-		// Find the closest distance to a line (made from two points) to a given point.
-		gFloat closestDistToLine(const Coord2& line1, const Coord2& line2, const Coord2& point);
+	enum class AngleResult {
+		ACUTE,
+		PERPENDICULAR,
+		OBTUSE
+	};
+	// Find what kind of angle the minimum angle between two vectors is.
+	[[nodiscard]] AngleResult minAngle(const Coord2& vec1, const Coord2& vec2) noexcept;
 
-		enum class AngleResult {
-			ACUTE,
-			PERPENDICULAR,
-			OBTUSE
-		};
-		// Find what kind of angle the minimum angle between two vectors is.
-		AngleResult minAngle(const Coord2& vec1, const Coord2& vec2);
+	// Reflect a direction across a normal.
+	[[nodiscard]] constexpr Coord2 reflect(const Coord2& dir, const Coord2& norm) noexcept {
+		return dir - 2.0f * norm * dir.dot(norm);
+	}
 
-		// Reflect a direction across a normal.
-		Coord2 reflect(const Coord2& dir, const Coord2& norm);
+	// Check if a value is almost zero (if the given value is between positive and negative tolerance exclusive).
+	[[nodiscard]] constexpr bool almostZero(gFloat t, gFloat tolerance = constants::EPSILON) noexcept {
+		return -tolerance < t && t < tolerance;
+	}
+	// Check if two values are almost the same (if their difference is less than the given tolerance).
+	[[nodiscard]] constexpr bool almostEquals(gFloat a, gFloat b, gFloat tolerance = constants::EPSILON) noexcept {
+		const gFloat diff = a - b;
+		const gFloat abs = diff < 0 ? -diff : diff; // std::abs is not yet constexpr.
+		return abs < tolerance;
+	}
+	// Check if two vectors' values are almost the same (if their difference is less than the given tolerance). Tolerance is applied separately to both values.
+	[[nodiscard]] constexpr bool almostEquals(const Coord2& a, const Coord2& b, gFloat tolerance = constants::EPSILON) noexcept {
+		return almostEquals(a.x, b.x, tolerance) && almostEquals(a.y, b.y, tolerance);
+	}
 
-		// Check if a value is almost zero (if the given value is between positive and negative tolerance exclusive).
-		inline bool almostZero(const gFloat t, const gFloat tolerance = constants::EPSILON) {
-			return -tolerance < t && t < tolerance;
-		}
-		// Check if two values are almost the same (if their difference is less than the given tolerance).
-		inline bool almostEquals(const gFloat a, const gFloat b, const gFloat tolerance = constants::EPSILON) {
-			return std::abs(a - b) < tolerance;
-		}
-		// Check if two vectors' values are almost the same (if their difference is less than the given tolerance). Tolerance is applied separately to both values.
-		inline bool almostEquals(const Coord2& a, const Coord2& b, const gFloat tolerance = constants::EPSILON) {
-			return almostEquals(a.x, b.x, tolerance) && almostEquals(a.y, b.y, tolerance);
-		}
-		// Clamp a value between two bounds (inclusive).
-		template<typename T>
-		inline T clamp(const T val, const T bound_one, const T bound_two) {
-			return bound_one <= bound_two ? (val < bound_one ? bound_one : val > bound_two ? bound_two : val) :
-			                                (val < bound_two ? bound_two : val > bound_one ? bound_one : val);
-		}
-		// Check if a value is between two bounds (inclusive).
-		template<typename T>
-		inline bool isBetween(const T val, const T bound_one, const T bound_two) {
-			return bound_one <= bound_two ? bound_one <= val && val <= bound_two : bound_two <= val && val <= bound_one;
-		}
+	// Check if a value is between two bounds (inclusive).
+	template<typename T>
+	[[nodiscard]] constexpr bool isBetween(const T& val, const T& bound_one, const T& bound_two) noexcept {
+		return bound_one <= bound_two ? bound_one <= val && val <= bound_two : bound_two <= val && val <= bound_one;
 	}
 }
 
