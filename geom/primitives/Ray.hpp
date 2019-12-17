604a96ac0343bb8b779a87089e479852226ecd85
Claytorpedo <claytorpedo@gmail.com>
2019-12-16 20:49:21 -0800

    Constructor cleanup.

--- geom/primitives/Ray.hpp
+++ geom/primitives/Ray.hpp
@@ -8,12 +8,15 @@ namespace geom {
 	public:
 		Coord2 origin, dir;
 
-		Ray() : origin(), dir() {}
+		Ray() = default;
+		Ray(const Ray&) = default;
+		Ray& operator=(const Ray&) = default;
+		Ray(Ray&&) = default;
+		Ray& operator=(Ray&&) = default;
 		// Dir should be a normalized vector.
-		Ray(Coord2 origin, Coord2 dir) : origin(origin), dir(dir) {}
-		Ray(gFloat origin_x, gFloat origin_y, gFloat dir_x, gFloat dir_y)
+		constexpr Ray(Coord2 origin, Coord2 dir) noexcept : origin(origin), dir(dir) {}
+		constexpr Ray(gFloat origin_x, gFloat origin_y, gFloat dir_x, gFloat dir_y) noexcept
 			: origin(origin_x, origin_y), dir(dir_x, dir_y) {}
-		Ray(const Ray& o) : origin(o.origin), dir(o.dir) {}
 	};
 }
 #endif // INCLUDE_GEOM_RAY_HPP
