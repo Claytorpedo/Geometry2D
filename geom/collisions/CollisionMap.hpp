3f0605a21bd0fd965039b5af814f2a92fa663a96
Claytorpedo <claytorpedo@gmail.com>
2020-04-01 23:31:56 -0700

    Default CollisionMap destructor.

--- geom/collisions/CollisionMap.hpp
+++ geom/collisions/CollisionMap.hpp
@@ -7,15 +7,16 @@
 #include "../units.hpp"
 
 namespace geom {
-	class CollisionMap {
-	public:
-		virtual ~CollisionMap() {}
-		// Given a collider and its delta, return a set of shapes it may collide with.
-		virtual const std::vector<Collidable*> getColliding(const Collidable& collider, Coord2 delta) const = 0;
-		// Given a collider, return a set of shapes it may overlap with.
-		virtual const std::vector<Collidable*> getColliding(const Collidable& collider) const {
-			return getColliding(collider, Coord2(0, 0));
-		}
-	};
+class CollisionMap {
+public:
+	virtual ~CollisionMap() = default;
+	// Given a collider and its delta, return a set of shapes it may collide with.
+	virtual const std::vector<Collidable*> getColliding(const Collidable& collider, Coord2 delta) const = 0;
+	// Given a collider, return a set of shapes it may overlap with.
+	virtual const std::vector<Collidable*> getColliding(const Collidable& collider) const {
+		return getColliding(collider, Coord2(0, 0));
+	}
+};
+
 }
 #endif // INCLUDE_GEOM_COLLISION_MAP_HPP
