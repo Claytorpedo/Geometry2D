733e7ef7977186726c11a6cce478bb2b78d5d8ad
Claytorpedo <claytorpedo@gmail.com>
2020-04-23 23:28:12 -0700

    Clean up Wall.

--- geom/collisions/Wall.hpp
+++ geom/collisions/Wall.hpp
@@ -7,11 +7,15 @@
 namespace ctp {
 class Wall : public Collidable {
 public:
-	Wall(const ShapeContainer& shape);
-	Wall(const ShapeContainer& shape, Coord2 position);
-	~Wall();
-	Coord2 getPosition() const;
-	ConstShapeRef getCollider() const;
+	Wall() = delete;
+	Wall(const Wall&) = default;
+	Wall(Wall&&) = default;
+	Wall& operator=(const Wall&) = default;
+	Wall& operator=(Wall&&) = default;
+	Wall(ShapeContainer shape, Coord2 position = {}) noexcept : shape_{std::move(shape)}, position_{position} {}
+
+	Coord2 getPosition() const override { return position_; }
+	ConstShapeRef getCollider() const override { return shape_; }
 private:
 	ShapeContainer shape_;
 	Coord2 position_;
